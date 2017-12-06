35af3506612872607ba13f748485974d34d143f4
Alexandre Volkoff <alexandre.volkoff@gmail.om>
2017-12-06 19:17:22 +0100

    First commit
    I have done nothing..

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/185856.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log('response', response);
+    }
+  });
 
 });
