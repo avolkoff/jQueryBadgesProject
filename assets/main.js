79bbe2c94de4617be51a373ab0ee77be10ab97d1
Alexandre Volkoff <alexandre.volkoff@gmail.om>
2017-12-08 18:11:28 +0100

    First build

--- assets/main.js
+++ assets/main.js
@@ -2,10 +2,12 @@ $(function() {
 
   // your code will go here
   $.ajax({
-    url: 'https://www.codeschool.com/users/185856.json',
+    url: 'https://www.codeschool.com/users/sergiocruz.json',
     dataType: 'jsonp',
     success: function(response) {
-      console.log('response', response);
+    	for (var i = 0; i <= response.courses.completed.length; i++) {
+    		$("#badges").append("<div class='course'><h3>" + response.courses.completed[i].title + "</h3><img src='" + response.courses.completed[i].badge + "' /><a href='" + response.courses.completed[i].url + "' target='_blank' class='btn btn-primary'>See course</a></div>");
+    	}
     }
   });
 
